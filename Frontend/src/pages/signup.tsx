09531ef12f50254e96da982f87990f3f1c599b73
Lakshya Goyal <lakshyagoyal34@gmail.com>
2025-08-31 16:09:09 +0530

    Frontend: CyberPunk in signup Completed

--- Frontend/src/pages/signup.tsx
+++ Frontend/src/pages/signup.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
 
 interface UserTypes {
   name: string;
@@ -85,74 +87,98 @@ transition duration-300 mb-8
       >
         SIGN UP
       </span>
-      <div className="border border-white px-8 py-8 rounded-2xl">
-        <form
-          className="flex flex-col items-center gap-4"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-col text-center gap-2">
-            <label>Full Name</label>
-            <input
-              placeholder="Enter your Full Name"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="border border-white rounded-full px-2 py-2 text-center"
-            />
-          </div>
-          <div className="flex flex-col text-center gap-2">
-            <label>Email</label>
-            <input
-              placeholder="Enter your Email"
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="border border-white rounded-full px-2 py-2 text-center"
-            />
-          </div>
-          <div className="flex flex-col text-center gap-2">
-            <label>Password</label>
-            <input
-              placeholder="Enter your Password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="border border-white rounded-full px-2 py-2 text-center"
-            />
-          </div>
-          <div className="flex flex-col text-center gap-2">
-            <label>Confirm Password</label>
-            <input
-              placeholder="Confirm your Password"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="border border-white rounded-full px-2 py-2 text-center"
-            />
-          </div>
-          <div className="flex flex-col text-center gap-2">
-            <label>Profile Picture</label>
-            <input
-              type="file"
-              onChange={(e) =>
-                setProfilePic(e.target.files ? e.target.files[0] : null)
-              }
-              className="text-white"
-            />
-          </div>
-          <button
-              className="relative inline-block px-6 py-2 rounded-2xl text-white tracking-wider 
-             bg-black/70 backdrop-blur-md border-2 border-transparent
-             [background:linear-gradient(#0a0a0a,#0a0a0a) padding-box,linear-gradient(to right,#8b5cf6,#ec4899,#3b82f6) border-box] 
-             transition duration-300 ease-in-out 
-             hover:shadow-[0_0_20px_rgba(236,72,153,0.8)] 
-             hover:scale-105 
-             focus:outline-none focus:ring-2 focus:ring-pink-500/70"
+      <div className="relative rounded-2xl bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500">
+        <div className="absolute inset-0 rounded-2xl bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 blur-2xl opacity-80"></div>
+        <div className="absolute inset-0 rounded-2xl bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 blur-[80px] opacity-60"></div>
+        <div className="relative bg-gray-400/30 border border-transparent rounded-2xl w-full max-w-sm px-8 py-6 backdrop-blur-md shadow-lg">
+          <form
+            className="flex flex-col items-center gap-3"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col text-center gap-1">
+              <label className="flex flex-row items-center justify-center gap-2">
+                <FaUser/>
+                <span>Name</span>
+              </label>
+              <input
+                placeholder="Enter your Full Name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full max-w-md placeholder-white/60 border border-transparent rounded-full px-2 py-2 text-center bg-black/50 backdrop-blur-md [background:linear-gradient(#0a0a0a,#0a0a0a) padding-box,linear-gradient(to right,#8b5cf6,#ec4899,#3b82f6) border-box] focus:outline-none focus:ring-2 focus:ring-pink-500/70"
+              />
+            </div>
+            <div className="flex flex-col text-center gap-1">
+              <label className="flex flex-row items-center justify-center gap-2">
+                <FaEnvelope/>
+                <span>Email</span>
+              </label>
+              <input
+                placeholder="Enter your Email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full max-w-md placeholder-white/60 border border-transparent rounded-full px-2 py-2 text-center bg-black/50 backdrop-blur-md [background:linear-gradient(#0a0a0a,#0a0a0a) padding-box,linear-gradient(to right,#8b5cf6,#ec4899,#3b82f6) border-box] focus:outline-none focus:ring-2 focus:ring-pink-500/70"
+              />
+            </div>
+            <div className="flex flex-col text-center gap-1">
+              <label className="flex flex-row items-center justify-center gap-2">
+                <FaLock/>
+                <span>Password</span>
+              </label>
+              <input
+                placeholder="Enter your Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full max-w-md placeholder-white/60 border border-transparent rounded-full px-2 py-2 text-center bg-black/50 backdrop-blur-md [background:linear-gradient(#0a0a0a,#0a0a0a) padding-box,linear-gradient(to right,#8b5cf6,#ec4899,#3b82f6) border-box] focus:outline-none focus:ring-2 focus:ring-pink-500/70"
+              />
+            </div>
+            <div className="flex flex-col text-center gap-1">
+              <label className="flex flex-row items-center justify-center gap-2">
+                <FaLock/>
+                <span>Confirm Password</span>
+              </label>
+              <input
+                placeholder="Confirm your Password"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="w-full max-w-md placeholder-white/60 border border-transparent rounded-full px-2 py-2 text-center bg-black/50 backdrop-blur-md [background:linear-gradient(#0a0a0a,#0a0a0a) padding-box,linear-gradient(to right,#8b5cf6,#ec4899,#3b82f6) border-box] focus:outline-none focus:ring-2 focus:ring-pink-500/70"
+              />
+            </div>
+            <div className="relative w-full">
+              <input
+                type="file"
+                onChange={(e) => setProfilePic(e.target.files ? e.target.files[0] : null)}
+                className="w-full opacity-0 absolute inset-0 cursor-pointer"
+              />
+              <div className="px-1 py-1 rounded-xl bg-black/50 border border-transparent text-white text-center">
+                {profilePic ? profilePic.name : "Choose Profile Picture"}
+              </div>
+            </div>
+
+            <button
+              className="relative inline-block w-full sm:w-auto px-6 py-2 rounded-2xl text-white tracking-wider 
+              bg-black/70 backdrop-blur-md border-2 border-transparent
+              [background:linear-gradient(#0a0a0a,#0a0a0a) padding-box,linear-gradient(to right,#8b5cf6,#ec4899,#3b82f6) border-box] 
+              transition duration-300 ease-in-out 
+              hover:shadow-[0_0_20px_rgba(236,72,153,0.8)] 
+              hover:scale-105 
+              focus:outline-none focus:ring-2 focus:ring-pink-500/70"
             >
               SUBMIT
             </button>
-        </form>
+          </form>
+        </div>
       </div>
+      <span className="flex flex-row gap-2 text-sm">
+        Already Have An Account :-
+        <Link to="/login" className="flex flex-row items-center gap-1 text-red-400 transition duration-300 ease-in-out  hover:text-blue-300 ">
+          <FaUser/>
+          <span>LOGIN</span>
+        </Link>
+      </span>
     </div>
   );
 };
