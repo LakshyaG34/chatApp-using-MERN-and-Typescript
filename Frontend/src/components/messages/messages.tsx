578d9fab12c21208dab21adb013cfe3398b42402
Lakshya Goyal <lakshyagoyal34@gmail.com>
2025-09-03 23:46:26 +0530

    Modified again

--- Frontend/src/components/messages/messages.tsx
+++ Frontend/src/components/messages/messages.tsx
@@ -1,13 +1,15 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import UseGetMsg from "../../hooks/useGetMsg";
 import MsgFrame from "../msgFrame/msgFrame";
 import Message from "./message";
 import { useSocketContext } from "../../context/socketContext";
+import useConversation from "../../zustand/useConversation";
 
 const Messages = () =>{
     const {messages, loading} = UseGetMsg();
     const { socket } = useSocketContext(); // ⬅️ get socket
-    const [liveMessages, setLiveMessages] = useState(messages);
+    // const [liveMessages, setLiveMessages] = useState(messages);
+    const { setMessages } = useConversation(); // from Zustand
 
     const lastMsgRef = useRef<HTMLDivElement | null>(null);
 
@@ -15,23 +17,33 @@ const Messages = () =>{
         setTimeout(()=>{
             lastMsgRef.current?.scrollIntoView({behavior : "smooth"});
         }, 100);
-    }, [liveMessages]);
-    useEffect(() => {
-    if (!socket) return;
-    socket.on("newMessage", (newMsg) => {
-        setLiveMessages((prev) => [...prev, newMsg]);
-    });
-    return () => {
-        socket.off("newMessage");
-    };
-   }, [socket]);
+    }, [messages]);
+//     useEffect(() => {
+//     if (!socket) return;
+//     socket.on("newMessage", (newMsg) => {
+//         setLiveMessages((prev) => [...prev, newMsg]);
+//     });
+//     return () => {
+//         socket.off("newMessage");
+//     };
+//    }, [socket]);
+   // Listen for real-time messages
+   useEffect(() => {
+       if (!socket) return;
+       socket.on("newMessage", (newMsg) => {
+           setMessages([...messages, newMsg]); // update Zustand messages
+       });
+       return () => {
+           socket.off("newMessage");
+       };
+   }, [socket, messages, setMessages]);
 
     return (
         <div className="flex flex-col p-2 min-w-0">
             {
                 !loading &&
-                liveMessages.length > 0 &&
-                liveMessages.map((message) => (
+                messages.length > 0 &&
+                messages.map((message) => (
                     <div key={message._id}>
                         <Message message={message} />
                     </div>
@@ -43,7 +55,7 @@ const Messages = () =>{
             }
             {
                 !loading &&
-                liveMessages.length === 0 &&
+                messages.length === 0 &&
                 (
                     <p className="text-center">Send a Message to start the conversation</p>
                 )
